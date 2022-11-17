be761376460d8ad3abfae989c3170a5b3aea1f4b
Daniel Moser <dmfaes@gmail.com>
2022-11-17 13:56:53 -0700

    setup.py version fix

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ if sys.argv[-1] == "publish":
     sys.exit()
 
 setup(name='pyhdust',
-    version='1.5.4',
+    version='1.5.5-2',
     description=('Analysis tools for multi-technique astronomical data and '
         'hdust models'),
     url='http://pyhdust.readthedocs.io',
